398952e6b6504f2602347e7eb75b7faff70b7c59
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2023-08-08 18:57:55 +0300

    check controller status

--- sswa6restapi1/assets/finish.sh
+++ sswa6restapi1/assets/finish.sh
@@ -16,8 +16,14 @@ function lookupIngressLogs {
     return
 }
 
-
+# проверим, запускал ли пользователь prepare.sh ожидаем увидеть работающий ingress
 controllerStatus=$(kubectl get po -l app.kubernetes.io/component=controller -n ingress-nginx -o json 2>&1)
+#controllerStatus=$(echo "fail" 1 2>&1)
+
+# проверим, что controllerStatus это валидный json
+if [[ $(echo $controllerStatus | jq 2>&1) =~ "parse error:" ]]; then
+    controllerStatus=$(jq --null-input --arg error "$controllerStatus" '$ARGS.named')
+fi
 
 # получаем логи из ингреса
 logs=$(kubectl logs -n ingress-nginx -l app.kubernetes.io/name=ingress-nginx 2>&1 | tail -n 100)
