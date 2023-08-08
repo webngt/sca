6d08aa3c27a1e2dbc87894f3cccb5ca502a3aa7f
Alexander Rozhdestvensky <1690271+webngt@users.noreply.github.com>
2023-08-08 17:15:26 +0300

    chore more verbose comments

--- sswa6restapi1/assets/finish.sh
+++ sswa6restapi1/assets/finish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# функция проверяет наличие заданного шаблона в логе
+# функция проверяет наличие заданного шаблона в логе ингреса
 #
 # в переменной $1 передаем содержимое лога
 # в переменной $2 передаем шаблон
-function request {
+function lookupIngressLogs {
     while read line; do
         if [[ $line =~ $2 ]] ;then
             echo "true"
@@ -19,18 +19,24 @@ function request {
 # получаем логи из ингреса
 logs=$(kubectl logs -n ingress-nginx -l app.kubernetes.io/name=ingress-nginx 2>&1 | tail -n 100)
 
+# шаблоны ожидаемых записей в логе ингреса
 patternIp="GET /httpbin/ip HTTP/1.1\" 200 28 \"-\" \"curl"
 patternHtml="GET /httpbin/html HTTP/1.1\" 200 3741 \"-\" \"curl"
 patternDelay="GET /httpbin/delay/5 HTTP/1.1\" 200 344 \"-\" \"curl"
 patternPost="POST /httpbin/post HTTP/1.1\" 200 487 \"-\" \"curl"
 
 
-# проверяем был ли выполнен curl -v localhost:32100/httpbin/ip
-hasIpRequest=$(request "$logs" "$patternIp")
-hasHtmlRequest=$(request "$logs" "$patternHtml")
-hasDelayRequest=$(request "$logs" "$patternDelay")
-hasPostRequest=$(request "$logs" "$patternPost")
+## проверки для curl, проверяем был ли использован curl на основе логов ингреса и шаблонов запроса
+# curl -v localhost:32100/httpbin/ip
+hasIpRequest=$(lookupIngressLogs "$logs" "$patternIp")
+# curl -v localhost:32100/httpbin/html
+hasHtmlRequest=$(lookupIngressLogs "$logs" "$patternHtml")
+# curl -v localhost:32100/httpbin/delay/5
+hasDelayRequest=$(lookupIngressLogs "$logs" "$patternDelay")
+# curl -v -XPOST localhost:32100/httpbin/post -H "Content-Type: application/json" -d'{"data":"test data here"}'
+hasPostRequest=$(lookupIngressLogs "$logs" "$patternPost")
 
+# собираем результирующий json
 jq --null-input \
 --arg didIpRequest "$hasIpRequest" \
 --arg didHtmlRequest "$hasHtmlRequest" \
